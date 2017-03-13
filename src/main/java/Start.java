ecc41824638f222cc133c4ca17d89dfcf4c2cff1
MBury <mbury1@mion.elka.pw.edu.pl>
2017-03-13 21:16:20 +0100

    final test

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -11,11 +11,15 @@ public class Start extends Application {
     };
 
     public void start(Stage stage) throws Exception {
+<<<<<<< Updated upstream
 <<<<<<< HEAD
     System.out.print("Hello world!!\n test test test test \n");
 =======
     System.out.print("Hello world!!\n commit bes test");
 >>>>>>> 88d288c... test lap
+=======
+    System.out.print("Hello world ok");
+>>>>>>> Stashed changes
 
         stage.setTitle("My JavaFX Application");
         //stage.setScene(scene);
