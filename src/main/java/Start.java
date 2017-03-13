8a30c9855cc56bdb4901c25ef4661d8eedb1b28d
MBury <mbury1@mion.elka.pw.edu.pl>
2017-03-13 15:47:34 +0100

    ok

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -11,7 +11,7 @@ public class Start extends Application {
     };
 
     public void start(Stage stage) throws Exception {
-    System.out.print("Hello world!!\n Avatar test");
+    System.out.print("Hello world!!\n commit");
 
         stage.setTitle("My JavaFX Application");
         //stage.setScene(scene);
